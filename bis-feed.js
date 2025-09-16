dc8af055ec2ec84b18ffd8980377649ab8fff30e
actions <actions@github.com>
2025-09-16 18:33:54 +0000

    Auto-update bis-feed.js

--- bis-feed.js
+++ bis-feed.js
@@ -1,7 +1,7 @@
 window.BIS_FEED = {
   "meta": {
     "season": "TWW S3",
-    "updated": "2025-09-16T12:38:31.768Z"
+    "updated": "2025-09-16T18:33:54.062Z"
   },
   "labels": {
     "Warrior": {
